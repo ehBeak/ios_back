b3a831f6d6a09166446277fad3311564cba54849
eunhee <dmsgml9903@gmail.com>
2021-12-09 17:17:04 +0900

    초기 세팅

---
+++ ios_back/src/main/java/com/example/ios_back/IosBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ios_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IosBackApplication.class, args);
+	}
+
+}
