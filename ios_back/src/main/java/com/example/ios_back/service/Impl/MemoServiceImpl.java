eedd4e88330c74efd0d436ceda24978ba67445f3
eunhee <dmsgml9903@gmail.com>
2022-02-07 17:09:47 +0900

    [FEAT] Memo 저장

---
+++ ios_back/src/main/java/com/example/ios_back/service/Impl/MemoServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.ios_back.service.Impl;
+
+import com.example.ios_back.domain.Memo;
+import com.example.ios_back.domain.Schedule;
+import com.example.ios_back.repository.MemoRepository;
+import com.example.ios_back.repository.ScheduleRepository;
+import com.example.ios_back.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemoServiceImpl implements MemoService {
+
+    private final MemoRepository memoRepository;
+    private final ScheduleRepository scheduleRepository;
+
+    //TODO: 2022.02.07 매개변수에 대한 null 처리
+
+    @Override
+    @Transactional
+    public void saveMemo(Long scheduleId, String content) {
+        Optional<Schedule> optionalSchedule = scheduleRepository.findById(scheduleId);
+        Schedule schedule = optionalSchedule.orElseThrow(() -> new NoSuchElementException());
+
+        Memo memo = Memo.createMemo(schedule);
+        memo.changeContent(content);
+
+        memoRepository.save(memo);
+    }
+}
---
+++ ios_back/src/main/java/com/example/ios_back/service/MemoService.java
@@ -0,0 +1,19 @@
+package com.example.ios_back.service;
+
+public interface MemoService {
+    /*
+     * 메모 수정
+     * */
+
+    /*
+     * 메모 조회
+     * */
+
+    /*
+     * 메모 저장
+     * */
+    public void saveMemo(Long scheduleId, String content);
+
+
+
+}
