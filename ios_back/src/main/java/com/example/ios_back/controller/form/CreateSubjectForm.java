c45f21a537fe1027e4e5e2074d5d65c8e8530300
eunhee <dmsgml9903@gmail.com>
2022-02-09 17:13:15 +0900

    [DEL] form에서 Setter제거

--- ios_back/src/main/java/com/example/ios_back/controller/form/CreateSubjectForm.java
+++ ios_back/src/main/java/com/example/ios_back/controller/form/CreateSubjectForm.java
@@ -1,10 +1,8 @@
 package com.example.ios_back.controller.form;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class CreateSubjectForm {
     private String subjectName;
     private String date;
